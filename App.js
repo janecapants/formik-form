47e03fb986fc4302f9d11fe1e773a6e19fcdc31e
Jane Capants <78875579+janecapants@users.noreply.github.com>
2022-01-18 13:59:02 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import {useFormik} from 'formik'
+
+
+
+function App() {
+  const formik = useFormik({
+    initialValues:{
+      email: '',
+      password: '', 
+    },
+    onSubmit: values =>{
+      console.log('form:', values);
+      alert("Login Successful")
+    },
+    
+    validate: values =>{
+      let errors = {};
+      if(!values.email) errors.email= 'Field Required';
+      if(!values.password) errors.password = 'Field Required'
+        return errors;
+        },
+     
+  });
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Email</div>
+        <input id='emailField' type='text' name='email' onChange={formik.handleChange}
+        value={formik.values.email}/>
+         {formik.errors.email ? ( <div id='emailError' style={{color:'red'}}>
+          {formik.errors.email}</div>): null}
+        <div>Password</div>
+        <input id='pswField' type='text' name='password' onChange={formik.handleChange}
+        value={formik.values.password}/>
+         {formik.errors.password ? ( <div id='pswError' style={{color:'red'}}>
+          {formik.errors.password}</div> ): null}
+        <button id='submitBtn' type ='button'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
